6fa6570e65f3c0db30b63ddcee30c0bed20abd61
jp9124 <jungpark.2002@gmail.com>
2026-09-04 17:02:02 -0600

    fixed ML4E algs for left side

--- src/puzzles/algs/pyraminxMl4eLeft.js
+++ src/puzzles/algs/pyraminxMl4eLeft.js
@@ -5,7 +5,7 @@
 const cases = [
   // Last 3 Edges
   { name: "Sledge", alg: "L U' R' L R L' L'", group: "Last 3 Edges" },
-  { name: "Hedge", alg: "U' L' U R U' R' L", group: "Last 3 Edges" },
+  { name: "Hedge", alg: "U' L U' R U R' L'", group: "Last 3 Edges" },
   { name: "Clockwise Cycle", alg: "U' L L R' L' R L'", group: "Last 3 Edges" },
   { name: "Counterclockwise Cycle", alg: "U' L R' L R L' L'", group: "Last 3 Edges" },
   { name: "Righty", alg: "L U' L'", group: "Last 3 Edges" },
@@ -25,22 +25,22 @@ const cases = [
   // Separated Bar
   { name: "Good Niky", alg: "L R U R' L'", group: "Separated Bar" },
   { name: "Good Sochi", alg: "L R U' R' L'", group: "Separated Bar" },
-  { name: "Super Sledge", alg: "L R U R' R' L R L' U L'", group: "Separated Bar" },
-  { name: "Super Hedge", alg: "U' R' R' L R L' U R L R U' R' R' L R L' U L'", group: "Separated Bar" },
-  { name: "Bad Niky", alg: "R U' R' U' L' U L", group: "Separated Bar" },
-  { name: "Bad Sochi", alg: "L' U L U R U' R'", group: "Separated Bar" },
+  { name: "Super Sledge", alg: "L U S L'", group: "Separated Bar" },
+  { name: "Super Hedge", alg: "U' L L R' L' R L' U' R U' R'", group: "Separated Bar" },
+  { name: "Bad Niky", alg: "U L U R U' R' U' L'", group: "Separated Bar" },
+  { name: "Bad Sochi", alg: "L U R U R' U' L'", group: "Separated Bar" },
   // Connected Bar
-  { name: "Right Spam", alg: "R U R' U R' L R L'", group: "Connected Bar" },
-  { name: "Left Spam", alg: "L' U' L U' L R' L' R", group: "Connected Bar" },
-  { name: "Bad Sledge", alg: "L R' L' R U' R U' R'", group: "Connected Bar" },
-  { name: "Bad Hedge", alg: "R' L R L' U L' U L", group: "Connected Bar" },
+  { name: "Right Spam", alg: "L U R U' R' U L'", group: "Connected Bar" },
+  { name: "Left Spam", alg: "L R U' R' R' L R L' U' L'", group: "Connected Bar" },
+  { name: "Bad Sledge", alg: "L R U R' U L'", group: "Connected Bar" },
+  { name: "Bad Hedge", alg: "L U L R' L' R R U R' L'", group: "Connected Bar" },
   // No Bar
-  { name: "Bad Sexy", alg: "L' U' L U' R U' R'", group: "No Bar" },
-  { name: "Bad Ugly", alg: "R U R' U L' U L", group: "No Bar" },
-  { name: "Bad Righty", alg: "L' U L U' R U R'", group: "No Bar" },
-  { name: "Bad Lefty", alg: "R U' R' U L' U' L", group: "No Bar" },
-  { name: "Double Sexy", alg: "R U' R' U' R U R'", group: "No Bar" },
-  { name: "Double Ugly", alg: "L' U L U L' U' L", group: "No Bar" },
+  { name: "Bad Sexy", alg: "U L L R' L' R R U' R' L'", group: "No Bar" },
+  { name: "Bad Ugly", alg: "L R U R' R' L R L' L'", group: "No Bar" },
+  { name: "Bad Righty", alg: "L U' L R' L' R U' L'", group: "No Bar" },
+  { name: "Bad Lefty", alg: "L U R' L R L' U L'", group: "No Bar" },
+  { name: "Double Sexy", alg: "L U' L' U' L U L'", group: "No Bar" },
+  { name: "Double Ugly", alg: "U L R' L R L' L' U' L U L'", group: "No Bar" },
 ];
 
 export default {
